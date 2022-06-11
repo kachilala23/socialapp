776ca3456cbd90295991cff3a3e39708e5ba6326
odumade <104792577+odumade@users.noreply.github.com>
2022-06-11 04:31:52 +0100

    connected the backend to frontend

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,14 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
 
         }
 
@@ -50,9 +58,13 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
+
+
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
